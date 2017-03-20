88139cd1c5a5187519883aa49e148ed82f65022d
mukund0299 <mukund0299@gmail.com>
2017-03-21 07:11:35 +0900

    Basic Files
    Added first necessary files and directories

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'stats_python',
+      version = '0.1',
+      description = '''A basic statistics package to compute simple confidence
+      intervals and stats tests''',
+      url = "https://github.com/mukund0299/stats_python",
+      author = 'mukun0299',
+      packages = ['stats_python']
+      )
+      
