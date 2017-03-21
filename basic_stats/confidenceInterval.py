11c8b6a1e3323de02bdec98b082cd041ddec02f5
mukund0299 <mukund0299@gmail.com>
2017-03-21 17:07:35 +0900

    Started Defining One Sample Z Interval

--- basic_stats/confidenceInterval.py
+++ basic_stats/confidenceInterval.py
@@ -1,4 +1,16 @@
 import openpyxl
 
-def oneSampleZInterval:
-    
+#One Sample Z Interval
+def oneSampleZInterval():
+    #path of excel file
+    src = input('Enter path of excel file\n')
+    try:
+        wb = openpyxl.load_workbook(src)
+    except(FileNotFoundError):
+        print('Path is incorrect')
+    #Set sheet
+    sheet = input('Enter sheet name')
+    wb.se
+    #Confidence Level
+    confidence = input('Enter confidence level in percent\n')
+    column = input('Enter column name \n')
