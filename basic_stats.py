b99d03798789c62e1b63430ea5cd36a64d4fd633
mukund0299 <mukund0299@gmail.com>
2017-03-28 17:26:07 +0900

    Fixed Formatting and added more statements

--- basic_stats.py
+++ basic_stats.py
@@ -9,7 +9,7 @@ import statistics
 def invNorm(area):
     #Setting initial result to zero
 	result = 0
-    #The lower bound is set to -3 because it's quite unlikely to have something
+    #The lower bound is set to -3 because it's unlikely to have something
     #higher than that
 	i = -3
     #If the result is not between area-0.0001 and area+0.0001
@@ -22,10 +22,15 @@ def invNorm(area):
 
 #Confidence Intervals
 def oneSampleZInterval(src, setSheet, confidenceLevel, column):
-    #Open the workbook
+    #Open the workbook and set the active sheet
     try:
         wb = openpyxl.load_workbook(src)
+		sheet = wb.get_sheet_by_name(setSheet)
+		index = openpyxl.cell.column_index_from_string(column)
     except FileNotFoundError:
         return 'File not found'
+	except KeyError:
+		return 'Sheet name not found'
+
     zCriticalNeg = invNorm((1-(confidenceLevel/100))/2)
     zCritical = -1 * zCriticalNeg
